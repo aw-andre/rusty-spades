f79bd3c16644acc29027a462170addfb8979a792
aw-andre <aw.andregerard@gmail.com>
2025-03-21 17:12:17 -0400

    feat(inputprocessor): add file validation and iterator conversion

--- src/inputprocessor/mod.rs
+++ src/inputprocessor/mod.rs
@@ -1,4 +1,28 @@
-use super::config;
+use super::config::Config;
+use std::path::Path;
 use std::fs::File;
+use std::io::BufReader;
+use bio::io::fastq::{ Reader, Records };
 
+impl Config {
+    fn to_paths(&self) -> (&Path, &Path) {
+        let forward = Path::new(self.forward.as_deref().unwrap());
+        let reverse = Path::new(self.reverse.as_deref().unwrap());
+        (forward, reverse)
+    }
+
+    fn to_files(&self) -> (Reader<BufReader<File>>, Reader<BufReader<File>>) {
+        let (forward, reverse) = self.to_paths();
+        let forward = Reader::from_file(forward).expect("Forward file is invalid!");
+        let reverse = Reader::from_file(reverse).expect("Reverse file is invalid!");
+        (forward, reverse)
+    }
+
+    pub fn to_iterators(&self) -> (Records<BufReader<File>>, Records<BufReader<File>>) {
+        let (forward, reverse) = self.to_files();
+        let forward = forward.records();
+        let reverse = reverse.records();
+        (forward, reverse)
+    }
+}
 
